39e3b8d25e65358dd0a72b43ac80b7924386be84
nicole <15829617282@163.com>
2017-05-25 14:13:49 +0800

    ugilify js

--- webpack.config.js
+++ webpack.config.js
@@ -7,3 +7,20 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   }
 };
+
+var webpack = require('webpack');
+
+var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+module.exports = {
+  entry: './dist/bundle.js',
+  output: {
+    filename: './dist/bundle-min.js'
+  },
+  plugins: [
+    new uglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+}
